f3b87fc7aa470a2961b29d22bbe96bee97a6562c
hushuna <19945096907@136.com>
2019-10-12 14:16:14 +0800

    x

---
+++ src/main/java/com/zhiliao/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.zhiliao.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: hsn
+ * @description:
+ * @date: 2019/10/12 14:14
+ **/
+@RestController
+public class TestController {
+}
