af71f4ab84c92d35cfab9e876b2901a7b72079dd
hushuna <19945096907@136.com>
2019-10-12 14:23:48 +0800

    x

--- src/main/java/com/zhiliao/dao/TestDao.java
+++ src/main/java/com/zhiliao/dao/TestDao.java
@@ -1,9 +1,12 @@
 package com.zhiliao.dao;
 
+import org.apache.ibatis.annotations.Mapper;
+
 /**
  * @author: hsn
  * @description:
  * @date: 2019/10/12 14:19
  **/
+@Mapper
 public interface TestDao {
 }
