9db82e44b5d99fc80f31c07199c12d28612ccdeb
hushuna <19945096907@136.com>
2019-10-12 14:39:21 +0800

    x

--- src/main/java/com/zhiliao/ZhiliaoCourseApplication.java
+++ src/main/java/com/zhiliao/ZhiliaoCourseApplication.java
@@ -3,9 +3,10 @@ package com.zhiliao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-
+@EnableFeignClients
 @EnableEurekaClient
 @EnableScheduling
 @EnableTransactionManagement
