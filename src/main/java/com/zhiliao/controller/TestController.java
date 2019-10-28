c8b9cea4f9392756dfb4d34c2575617a5052faa5
hushuna <19945096907@136.com>
2019-10-28 10:34:42 +0800

    x

--- src/main/java/com/zhiliao/controller/TestController.java
+++ src/main/java/com/zhiliao/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.zhiliao.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,5 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
  * @date: 2019/10/12 14:14
  **/
 @RestController
+@RequestMapping(value = "/test")
 public class TestController {
+
+
+    @GetMapping(value = "getString")
+    public String getString(){
+        return "胡淑娜";
+    }
+
 }
